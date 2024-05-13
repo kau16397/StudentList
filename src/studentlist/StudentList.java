feff90dc33df2c5208a28d5d990a893be391fd8e
kau16397 <kau16397@sheridancollege.ca>
2024-05-13 15:09:22 -0400

    Student List is updated

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -23,8 +23,8 @@ public class StudentList {
   s2.setName("Arshnoor Kaur");
   Student s3 = new Student();
   s3.setName("Ritika");
-  Student s4 = new Student();
-  s4.setName("Ritika");
+   student s4 = new Student();
+   s4.setName("Ritika");
   
   studentList[0] = s1;
   studentList[1] = s2;
